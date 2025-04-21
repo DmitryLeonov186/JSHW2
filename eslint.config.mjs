11b61c21598b3c2bdf17db876b02647f40d4ab40
Dmitry Leonov <lemomonster@mail.ru>
2025-04-21 18:22:39 +0500

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([{
+    languageOptions: {
+        globals: {},
+    },
+
+    rules: {
+        "no-var": "error",
+        semi: "error",
+        indent: "error",
+        "no-multi-spaces": "error",
+        "space-in-parens": "error",
+        "no-multiple-empty-lines": "error",
+        "prefer-const": "error",
+        "no-use-before-define": "error",
+    },
+}]);
